81fc36343cc10bfa8bf8bb6049ed9fc7b7159fdb
Maria <maria.bilichenko@chem.uzh.ch>
2025-07-08 12:30:33 +0000

    added install requires

--- setup.py
+++ setup.py
@@ -4,4 +4,11 @@ setup(
     name="kaggle_common",
     version="0.1.0",
     packages=find_packages(include=["common", "common.*"]),    # this will pick up common/ and utils/
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn>=1.0",
+        "category_encoders>=2.6.0",   
+        # …other deps…
+    ],
 )
